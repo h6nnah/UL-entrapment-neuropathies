32f61037168cbc2f2be35bd16e00e04a1a02022d
Hannah Erdevig <4206998+h6nnah@users.noreply.github.com>
2021-12-08 18:28:50 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+$(document).ready(function() {
+  $("input:radio[name=nerve]").click(function() {
+    var currentNerve = $("input:radio[name=nerve]:checked").val();
+    var image_name = "";
+    document.getElementById('test').innerHTML = 'TEST'
+    switch (currentNerve) {
+      case 'Median':
+        document.getElementById('test').innerHTML = 'MEDIAN';
+        break;
+      case 'Ulnar':
+        document.getElementById('test').innerHTML = 'ULNAR';
+        break;
+      case 'Radial':
+        document.getElementById('test').innerHTML = 'RADIAL';
+        break;
+      default:
+        image_name += "mediandefault";
+        break;
+    }
+    
+  });
+});    
+        
+function updateNerve() {
+  switch (document.getElementByName('nerve') {
+    case 'median' = 'MEDIAN';
+}
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
+function other() {
+  var currentNerve = form.nerve.
+  switch (document.getElementsByName(nerve)) {
+    case document.getElementById('median').checked:
+      document.getElementById('test').innerHTML = 'MEDIAN';
+      break;
+    case document.getElementById('ulnar').checked:
+      day = "Monday";
+      break;
+    case document.getElementById('radial').checked:
+       day = "Tuesday";
+      break;
+  }
 }
