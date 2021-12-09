50648d8ca8ca85b30c52fa2e4bf5c5152f4a7657
Hannah Erdevig <4206998+h6nnah@users.noreply.github.com>
2021-12-08 21:35:30 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,15 @@
-$(document).ready(function() {
-  $("input:radio[name=nerve]").click(function() {
-    var currentNerve = $("input:radio[name=nerve]:checked").val();
-    var image_name = "";
-    document.getElementById('test').innerHTML = 'TEST'
-    switch (currentNerve) {
-      case 'Median':
-        document.getElementById('test').innerHTML = 'MEDIAN';
-        break;
-      case 'Ulnar':
-        document.getElementById('test').innerHTML = 'ULNAR';
-        break;
-      case 'Radial':
-        document.getElementById('test').innerHTML = 'RADIAL';
-        break;
-      default:
-        image_name += "mediandefault";
-        break;
-    }
-    
-  });
-});    
-        
-function updateNerve() {
-  switch (document.getElementByName('nerve') {
-    case 'median' = 'MEDIAN';
-}
-
-function other() {
-  var currentNerve = form.nerve.
-  switch (document.getElementsByName(nerve)) {
-    case document.getElementById('median').checked:
-      document.getElementById('test').innerHTML = 'MEDIAN';
-      break;
-    case document.getElementById('ulnar').checked:
-      day = "Monday";
-      break;
-    case document.getElementById('radial').checked:
-       day = "Tuesday";
-      break;
-  }
+var ImgArray = [
+  "https://www.bartleby.com/107/Images/large/image816.gif",
+  "https://www.bartleby.com/107/Images/large/image816.gif",
+ "https://www.bartleby.com/107/Images/large/image818.gif"
+];
+var TitleArray = [
+  "Median Nerve",
+  "Ulnar Nerve",
+  "Radial Nerve"
+];
+function UpdateImg(imgSrc, imgId) {
+  document.getElementById('NerveImg').src = ImgArray[imgSrc];
+  document.getElementById('NerveImg').alt = imgId;
+  document.getElementById('nerveTitle').innerHTML = TitleArray[imgSrc];
 }
