7ff8c6735da58452fb820016371120356e9ae628
Hannah Erdevig <4206998+h6nnah@users.noreply.github.com>
2021-12-11 23:43:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,46 @@ var ImgArray = [
   "https://www.bartleby.com/107/Images/large/image816.gif",
  "https://www.bartleby.com/107/Images/large/image818.gif"
 ];
+
 var TitleArray = [
   "Median Nerve",
+  "Carpal Tunnel",
+  "Thoracic Outlet Syndrome"
   "Ulnar Nerve",
   "Radial Nerve"
 ];
-function UpdateImg(imgSrc, imgId) {
-  document.getElementById('NerveImg').src = ImgArray[imgSrc];
-  document.getElementById('NerveImg').alt = imgId;
-  document.getElementById('nerveTitle').innerHTML = TitleArray[imgSrc];
+
+function updateNerve(nerveVal, nerveId) {
+  var nerveVal = nerveVal
+  document.getElementById('nerveImg').src = ImgArray[nerveVal];
+  document.getElementById('nerveImg').alt = nerveId;
+  document.getElementById('nerveTitle').innerHTML = TitleArray[nerveVal];
 }
+
+function updateLesion() {
+   var x = document.getElementById('lesionSlider').value;
+  document.getElementById(lesionTitle).innerHTML = x;
+}
+
+const pos = { x : 0, y : 0 };
+
+document.addEventListener('mousemove', e => { saveCursorPosition(e.clientX, e.clientY); 
+})
+                                             
+pos.x = (x / window.innerWidth).toFixed(2);
+pos.y = (y / window.innerHeight).toFixed(2);
+                                             
+                                             document.documentElement.style.setProperty('--x', pos.x);
+document.documentElement.style.setProperty('--y', pos.y);
+                                             
+const pos = { x : 0, y : 0 };
+
+const saveCursorPosition = function(x, y) {
+    pos.x = (x / window.innerWidth).toFixed(2);
+    pos.y = (y / window.innerHeight).toFixed(2);
+    document.documentElement.style.setProperty('--x', pos.x);
+    document.documentElement.style.setProperty('--y', pos.y);
+}
+
+document.addEventListener('mousemove', e => { saveCursorPosition(e.clientX, e.clientY); 
+})
