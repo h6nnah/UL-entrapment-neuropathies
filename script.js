5fcbbc901fbf055a07127503c08263329dbe6b21
Hannah Erdevig <4206998+h6nnah@users.noreply.github.com>
2021-12-14 00:52:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -14,31 +14,8 @@ var TitleArray = [
   "Transverse metacarpal ligament (Bowler's thumb)"
 ];
 
-function updateNerve(nerveVal, nerveId) {
-  document.getElementById('nerveImg').src = ImgArray[nerveVal];
-  document.getElementById('nerveImg').alt = nerveId;
-  document.getElementById('nerveTitle').innerHTML = TitleArray[nerveVal];
+function updateE(EVal, EId) {
+  document.getElementById('EImg').src = ImgArray[EVal];
+  document.getElementById('EImg').alt = EId;
+  document.getElementById('ETitle').innerHTML = TitleArray[EVal];
 }
-
-const pos = { x : 0, y : 0 };
-
-document.addEventListener('mousemove', e => { saveCursorPosition(e.clientX, e.clientY); 
-})
-                                             
-pos.x = (x / window.innerWidth).toFixed(2);
-pos.y = (y / window.innerHeight).toFixed(2);
-                                             
-                                             document.documentElement.style.setProperty('--x', pos.x);
-document.documentElement.style.setProperty('--y', pos.y);
-                                             
-const pos = { x : 0, y : 0 };
-
-const saveCursorPosition = function(x, y) {
-    pos.x = (x / window.innerWidth).toFixed(2);
-    pos.y = (y / window.innerHeight).toFixed(2);
-    document.documentElement.style.setProperty('--x', pos.x);
-    document.documentElement.style.setProperty('--y', pos.y);
-}
-
-document.addEventListener('mousemove', e => { saveCursorPosition(e.clientX, e.clientY); 
-})
