0ed1dc4e8c9998ad9490341efee1c742ad5bb97d
Hannah Erdevig <4206998+h6nnah@users.noreply.github.com>
2021-12-14 01:38:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,23 +3,34 @@ var ImgArray = [
   "https://www.bartleby.com/107/Images/large/image816.gif",
   "https://www.bartleby.com/107/Images/large/image818.gif",
   "full.png",
-  "full.png",
 ];
 
 var TitleArray = [
   "Ligament of Struthers",
-  "Pronator teres m. (Pronator syndrome)",
-  "Flexor pollicis longus m. (Anterior interosseous syndrome)",
-  "Transverse carpal ligament (Carpal tunnel syndrome)",
-  "Transverse metacarpal ligament (Bowler's thumb)"
+  "Pronator teres syndrome",
+  "Anterior interosseous syndrome",
+  "Transverse carpal ligament (Carpal tunnel syndrome)"
 ];
 
 var MotorArray = [
-  "1, 2, <br> 3, 4, 5",
-  "1, 2, 3, 4",
-  "1, 2, 3",
-  "1, 2",
-  "1"
+  "pronator teres m., flexor pollicis longus m., abductor pollicis brevis m., flexor digitorum profundus m. to digits 2 and 3, and opponens pollicis m.",
+  "flexor pollicis longus m., abductor pollicis brevis m., flexor digitorum profundus m. to digits 2 and 3, and opponens pollicis m.",
+  "Flexor pollicis longus m.",
+  "Flexor pollicis brevis m. (superficial head), abductor pollicis brevis m., opponens pollicis m., Lumbrical mm. to digits 2 and 3"
+];
+
+var BranchArray = [
+  "",
+  "flexor pollicis longus m., abductor pollicis brevis m., flexor digitorum profundus m. to digits 2 and 3, and opponens pollicis m.",
+  "Anterior interosseus n. from median n.",
+  "recurrent branch of median n."
+];
+
+var SignArray = [
+  "Hand of benediction",
+  "",
+  "",
+  "ape hand"
 ];
 
 function updateE(EVal) {
